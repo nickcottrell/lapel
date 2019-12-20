a9e526b1d112e660604a7d925ee02c56f928b93b
Nick Cottrell <nick.a.cottrell@gmail.com>
2019-12-19 21:49:34 -0800

    should be const

--- demo-INTERFACE.js
+++ demo-INTERFACE.js
@@ -1,14 +1,6 @@
-/*let QUESTION = "Name a fruit that rhymes with ape.";
-let CORRECT_ANSWER = "GRAPE";
-let YES_MESSAGE = "congratulations. grape is the magic word. You win.";
-let NO_MESSAGE = "incorrect. that does not RIME with ape. click the try again button";*/
+const QUESTION = "have you ever been camping?";
+const QUESTION_TIME = 1000; // this is the time it takes to read the item... TODO, calulate this value based on the string length.
 
-
-let QUESTION = "have you ever been camping?";
-let QUESTION_TIME = 1000; // this is the time it takes to read the item... TODO, calulate this value based on the string length.
-
-let CORRECT_ANSWER = "YES";
-let YES_MESSAGE = "That's great. I like Yosemiiiteeeee...";
-let NO_MESSAGE = "That's too bad. There are so many great places to see.";
-
-
+const CORRECT_ANSWER = "YES";
+const YES_MESSAGE = "That's great. I like Yosemiiiteeeee...";
+const NO_MESSAGE = "That's too bad. There are so many great places to see.";
